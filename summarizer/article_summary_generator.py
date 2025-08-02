a62c60c641a0abb307cf1dd94c5efa4214181511
Masalovich Viacheslav <masalovich.viacheslav@gmail.com>
2025-08-02 08:44:18 +0300

    refactor(article_summary_generator): modify summarize_material func in view of input structure change
    Initially article title extraction was delegated to LLM, but LLM is always trying to generate
    title instead of its extraction from web-page. Approach was changed and now title is extracted
    during subscription email parsing. It led to input/output structure changes through some modules:
    material_sources_extractor, redirect_url_resolver. Corresponding changes are made in article_summary_generator.

--- summarizer/article_summary_generator.py
+++ summarizer/article_summary_generator.py
@@ -12,16 +12,17 @@ from summarizer.prompts import SNIPPET_ANALYSIS_PROMPT, ARTICLE_ANALYSIS_PROMPT
 load_dotenv()
 gemini_api_key = os.getenv("GEMINI_API_KEY")
 
-def summarize_material(materials: dict[str, list[str]]) -> dict[str, list[dict[str, str]]]:
+def summarize_material(materials: dict[str, list[str]|dict[str, str]]) -> dict[str, list[dict[str, str]]]:
     """
     Generates summaries and tags for given materials (articles or PyTricks) using Gemini API.
 
-    This function iterates over the provided materials, sends them to the Gemini model
+    This function iterates over the provided material types, sends them to the Gemini model
     with specific prompts, and parses the JSON responses. Only valid responses with required
     keys are kept and returned.
 
-    :param materials: a dictionary with keys like 'articles' or 'pytricks',
-    and values — lists of article URLs or code snippets accordingly
+    :param materials: a dictionary with keys like 'articles' or 'pytricks', and values —
+    dictionary of 'article title-urls' pairs or an empty dictionary and list of snippets or
+    an empty list respectively
     :return: a dictionary with the same keys ('articles' or 'pytricks'),
     where each value is a list of parsed and validated JSON responses from Gemini
     """
@@ -30,16 +31,20 @@ def summarize_material(materials: dict[str, list[str]]) -> dict[str, list[dict[s
     try:
         client = genai.Client(api_key=gemini_api_key)
 
-        for material_type, material_sources in materials.items():
-            for request_number, material_source in enumerate(material_sources, start=1):
+        request_number = 0
+
+        if materials['articles']:
+            articles = materials['articles']
+
+            for index, (article_title, link_to_article) in enumerate(articles.items(), start=request_number + 1):
+                request_number = index
                 # bypassing the requests per minute limit (10 req/min)
                 if request_number % 10 == 1 and request_number != 1:
                     time.sleep(60)
 
-                if material_type == 'articles':
                     response = client.models.generate_content(
                         model='gemini-2.5-flash',
-                        contents=ARTICLE_ANALYSIS_PROMPT.format(url=material_source)
+                        contents=ARTICLE_ANALYSIS_PROMPT.format(url=link_to_article)
                     )
                     # checking if the model response has JSON format
                     try:
@@ -48,21 +53,29 @@ def summarize_material(materials: dict[str, list[str]]) -> dict[str, list[dict[s
                         try:
                             # checking if all required content is provided by the model
                             if all((
-                                    decoded_response['article name'],
                                     decoded_response['article summary'],
                                     decoded_response['tags']
                             )):
-                                decoded_response['url'] = material_source
+                                decoded_response['article_title'] = article_title
+                                decoded_response['url'] = link_to_article
                                 materials_with_summaries['articles'].append(decoded_response)
                         except KeyError as e:
                             print(f'No key in the model response as required by prompt: {e}')
                     except JSONDecodeError as e:
                         print(f'The model response has not JSON format as required by prompt: {e}\n{response.text}')
 
-                elif material_type == 'pytricks':
+        if materials['pytricks']:
+            pytricks = materials['pytricks']
+
+            for index, snippet in enumerate(pytricks, start=request_number + 1):
+                request_number = index
+                # bypassing the requests per minute limit (10 req/min)
+                if request_number % 10 == 1 and request_number != 1:
+                    time.sleep(60)
+
                     response = client.models.generate_content(
                         model='gemini-2.5-flash',
-                        contents=SNIPPET_ANALYSIS_PROMPT.format(code=material_source)
+                        contents=SNIPPET_ANALYSIS_PROMPT.format(code=snippet)
                     )
                     # checking if the model response has JSON format
                     try:
@@ -74,7 +87,7 @@ def summarize_material(materials: dict[str, list[str]]) -> dict[str, list[dict[s
                                     decoded_response['snippet summary'],
                                     decoded_response['tags']
                             )):
-                                decoded_response['snippet'] = material_source
+                                decoded_response['snippet'] = snippet
                                 materials_with_summaries['pytricks'].append(decoded_response)
                         except KeyError as e:
                             print(f'No key in the model response as required by prompt: {e}')
