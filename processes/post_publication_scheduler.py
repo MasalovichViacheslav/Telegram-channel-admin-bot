0fa61eb2bffff3df87d3e593d72ecc911f889473
Masalovich Viacheslav <masalovich.viacheslav@gmail.com>
2025-09-08 18:03:02 +0300

    chore(post_publication_scheduler): add logging with info level

--- processes/post_publication_scheduler.py
+++ processes/post_publication_scheduler.py
@@ -3,10 +3,13 @@ from zoneinfo import ZoneInfo
 from db_connector.db_cursor_creator import get_db_cursor
 from post_storage.pg_storage_manager import move_posts_to_current_batch
 from scheduler.publication_scheduler import calculate_publication_schedule, upload_schedule_to_db
+from utils.logging_config import log_json
 
 
 TZ = ZoneInfo('Europe/Minsk')
 WEEKDAY_TO_CREATE_NEW_SCHEDULE = 5
+
+LOGGER = 'POST PUBLICATIONS SCHEDULING PROCESS'
 
 def is_time_to_schedule_next_week_publications()-> bool | None:
     """
@@ -22,6 +25,8 @@ def is_time_to_schedule_next_week_publications()-> bool | None:
     :return: True if both conditions are met (specified weekday + empty schedule), False otherwise,
         None in case of DB connection failure
     """
+    log_json(LOGGER, 'info', 'Checking whether it\'s time to schedule next week publications or not')
+
     current_weekday = datetime.now(tz=TZ).isoweekday()
 
     with get_db_cursor() as cur:
@@ -35,8 +40,10 @@ def is_time_to_schedule_next_week_publications()-> bool | None:
             query_result = cur.fetchone()
 
             if current_weekday == WEEKDAY_TO_CREATE_NEW_SCHEDULE and not query_result:
+                log_json(LOGGER, 'info', 'It\'s time to schedule next week publications')
                 return True
             else:
+                log_json(LOGGER, 'info', 'Time to schedule next week publications has not come yet')
                 return False
 
 
@@ -54,10 +61,15 @@ def schedule_next_week_publications() -> None:
 
     :return: None
     """
+    log_json(LOGGER, 'info', 'The process is started')
+
     publication_qty = move_posts_to_current_batch()
     if not publication_qty:
+        log_json(LOGGER, 'info', 'The process is terminated',
+                 reason='There aren\'t post texts for new week publications')
         return
 
     schedule = calculate_publication_schedule(publication_qty)
-
     upload_schedule_to_db(schedule)
+
+    log_json(LOGGER, 'info', 'The process is ended')
